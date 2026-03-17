47506d785b70806781744da829d7d30695040d06
luisalvaradodev10s <luisalvaradodev10s@gmail.com>
2026-03-16 23:39:51 -0300

    chore: update product prices

--- src/data/products.js
+++ src/data/products.js
@@ -3,7 +3,7 @@ export const products = [
     id: '1',
     name: 'Galletón de Avena',
     description: 'Chips de chocolate • 10g Proteína • Sin azúcar',
-    price: 3500,
+    price: 900,
     image: 'https://images.unsplash.com/photo-1499636136210-6f4ee915583e?w=500&q=80', // Galleta de avena referencial
     category: 'Galletas'
   },
@@ -11,7 +11,7 @@ export const products = [
     id: '2',
     name: 'Twix Tiramisú',
     description: 'Dulce de leche • 15g Proteína • Sin azúcar',
-    price: 4500,
+    price: 1900,
     image: 'https://placehold.co/600x400/3A2C24/FFFFFF/png?text=Twix+Tiramisu', // Cambio provisorio a imagen fija
     category: 'Barras'
   },
@@ -19,7 +19,7 @@ export const products = [
     id: '3',
     name: 'Barra Cacao y Maní',
     description: '15g Proteína • Sin azúcar',
-    price: 4500,
+    price: 1900,
     image: 'https://images.unsplash.com/photo-1603569283847-aa295f0d016a?w=500&q=80', // Barra cacao referencial
     category: 'Barras'
   },
@@ -27,7 +27,7 @@ export const products = [
     id: '4',
     name: 'Barra Coco',
     description: '15g Proteína • Sin azúcar',
-    price: 4500,
+    price: 1900,
     image: 'https://placehold.co/600x400/F0EBE1/5C4033/png?text=Barra+Coco', // Cambio provisorio a imagen fija
     category: 'Barras'
   },
@@ -35,7 +35,7 @@ export const products = [
     id: '5',
     name: 'Twix Chocolate Cinnamon',
     description: 'Chocolate • canela • 15g Proteína • Sin azúcar',
-    price: 4500,
+    price: 1900,
     image: 'https://images.unsplash.com/photo-1549007953-2f2dc0b24019?w=500&q=80', // Textura barra referencial
     category: 'Barras'
   }
